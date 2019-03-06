a3a8e304a17ab4b4b97f998a531a5d22f488cd76
Aleksandr Dmitriev <dmitriev@gorodkolomna.ru>
2019-03-06 22:41:04 +0300

    pregnancy gallery

---
+++ assets/js/gallery.js
@@ -0,0 +1,37 @@
+document.getElementById('pregnancy-gallery').addEventListener('click', function () {
+    lightGallery(document.getElementById('pregnancy-gallery'), {
+        dynamic: true,
+        dynamicEl: [{
+            "src": './images/pregnancy/pregnancy1.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy2.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy3.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy4.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy5.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy6.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy7.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy8.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy9.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy10.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy11.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy12.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy13.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy14.jpg',
+        }, {
+            'src': './images/pregnancy/pregnancy15.jpg',    
+        }]
+    })
+
+});
