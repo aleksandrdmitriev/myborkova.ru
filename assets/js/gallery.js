43d065112b95b4493f8c7781a8946ea7dad8f125
Aleksandr Dmitriev <dmitriev@gorodkolomna.ru>
2019-03-07 00:16:29 +0300

    all galleries

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -31,6 +31,328 @@ document.getElementById('pregnancy-gallery').addEventListener('click', function 
             'src': './images/pregnancy/pregnancy14.jpg',
         }, {
             'src': './images/pregnancy/pregnancy15.jpg',    
+        }, {
+            'src': './images/pregnancy/pregnancy16.jpg',
+        }]
+    })
+
+});
+document.getElementById('baby-gallery').addEventListener('click', function () {
+    lightGallery(document.getElementById('baby-gallery'), {
+        dynamic: true,
+        dynamicEl: [{
+            "src": './images/baby/baby1.jpg',
+        }, {
+            'src': './images/baby/baby2.jpg',
+        }, {
+            'src': './images/baby/baby3.jpg',
+        }, {
+            'src': './images/baby/baby4.jpg',
+        }, {
+            'src': './images/baby/baby5.jpg',
+        }, {
+            'src': './images/baby/baby6.jpg',
+        }, {
+            'src': './images/baby/baby7.jpg',
+        }, {
+            'src': './images/baby/baby8.jpg',
+        }, {
+            'src': './images/baby/baby9.jpg',
+        }, {
+            'src': './images/baby/baby10.jpg',
+        }, {
+            'src': './images/baby/baby11.jpg',
+        }, {
+            'src': './images/baby/baby12.jpg',
+        }, {
+            'src': './images/baby/baby13.jpg',
+        }, {
+            'src': './images/baby/baby14.jpg',
+        }, {
+            'src': './images/baby/baby15.jpg',
+        }, {
+            'src': './images/baby/baby16.jpg',
+        }, {
+            'src': './images/baby/baby17.jpg',
+        }]
+    })
+
+});
+document.getElementById('afterone-gallery').addEventListener('click', function () {
+    lightGallery(document.getElementById('afterone-gallery'), {
+        dynamic: true,
+        dynamicEl: [{
+            "src": './images/afterone/afterone1.jpg',
+        }, {
+            'src': './images/afterone/afterone2.jpg',
+        }, {
+            'src': './images/afterone/afterone3.jpg',
+        }, {
+            'src': './images/afterone/afterone4.jpg',
+        }, {
+            'src': './images/afterone/afterone5.jpg',
+        }, {
+            'src': './images/afterone/afterone6.jpg',
+        }, {
+            'src': './images/afterone/afterone7.jpg',
+        }, {
+            'src': './images/afterone/afterone8.jpg',
+        }, {
+            'src': './images/afterone/afterone9.jpg',
+        }, {
+            'src': './images/afterone/afterone10.jpg',
+        }, {
+            'src': './images/afterone/afterone11.jpg',
+        }, {
+            'src': './images/afterone/afterone12.jpg',
+        }, {
+            'src': './images/afterone/afterone13.jpg',
+        }, {
+            'src': './images/afterone/afterone14.jpg',
+        }, {
+            'src': './images/afterone/afterone15.jpg',
+        }, {
+            'src': './images/afterone/afterone16.jpg',
+        }, {
+            'src': './images/afterone/afterone17.jpg',
+        }, {
+            'src': './images/afterone/afterone18.jpg',
+        }, {
+            'src': './images/afterone/afterone19.jpg',
+        }, {
+            'src': './images/afterone/afterone20.jpg',
+        }]
+    })
+
+});
+document.getElementById('baptism-gallery').addEventListener('click', function () {
+    lightGallery(document.getElementById('baptism-gallery'), {
+        dynamic: true,
+        dynamicEl: [{
+            "src": './images/baptism/baptism1.jpg',
+        }, {
+            'src': './images/baptism/baptism2.jpg',
+        }, {
+            'src': './images/baptism/baptism3.jpg',
+        }, {
+            'src': './images/baptism/baptism4.jpg',
+        }, {
+            'src': './images/baptism/baptism5.jpg',
+        }, {
+            'src': './images/baptism/baptism6.jpg',
+        }, {
+            'src': './images/baptism/baptism7.jpg',
+        }, {
+            'src': './images/baptism/baptism8.jpg',
+        }, {
+            'src': './images/baptism/baptism9.jpg',
+        }, {
+            'src': './images/baptism/baptism10.jpg',
+        }, {
+            'src': './images/baptism/baptism11.jpg',
+        }, {
+            'src': './images/baptism/baptism12.jpg',
+        }, {
+            'src': './images/baptism/baptism13.jpg',
+        }]
+    })
+
+});
+document.getElementById('family-gallery').addEventListener('click', function () {
+    lightGallery(document.getElementById('family-gallery'), {
+        dynamic: true,
+        dynamicEl: [{
+            "src": './images/family/family1.jpg',
+        }, {
+            'src': './images/family/family2.jpg',
+        }, {
+            'src': './images/family/family3.jpg',
+        }, {
+            'src': './images/family/family4.jpg',
+        }, {
+            'src': './images/family/family5.jpg',
+        }, {
+            'src': './images/family/family6.jpg',
+        }, {
+            'src': './images/family/family7.jpg',
+        }, {
+            'src': './images/family/family8.jpg',
+        }, {
+            'src': './images/family/family9.jpg',
+        }, {
+            'src': './images/family/family10.jpg',
+        }, {
+            'src': './images/family/family11.jpg',
+        }, {
+            'src': './images/family/family12.jpg',
+        }, {
+            'src': './images/family/family13.jpg',
+        }, {
+            'src': './images/family/family14.jpg',
+        }, {
+            'src': './images/family/family15.jpg',
+        }, {
+            'src': './images/family/family16.jpg',
+        }, {
+            'src': './images/family/family17.jpg',
+        }, {
+            'src': './images/family/family18.jpg',
+        }, {
+            'src': './images/family/family19.jpg',
+        }, {
+            'src': './images/family/family20.jpg',
+        }, {
+            'src': './images/family/family21.jpg',
+        }]
+    })
+
+});
+
+document.getElementById('events-gallery').addEventListener('click', function () {
+    lightGallery(document.getElementById('events-gallery'), {
+        dynamic: true,
+        dynamicEl: [{
+            "src": './images/events/events1.jpg',
+        }, {
+            'src': './images/events/events2.jpg',
+        }, {
+            'src': './images/events/events3.jpg',
+        }, {
+            'src': './images/events/events4.jpg',
+        }, {
+            'src': './images/events/events5.jpg',
+        }, {
+            'src': './images/events/events6.jpg',
+        }, {
+            'src': './images/events/events7.jpg',
+        }, {
+            'src': './images/events/events8.jpg',
+        }, {
+            'src': './images/events/events9.jpg',
+        }, {
+            'src': './images/events/events10.jpg',
+        }, {
+            'src': './images/events/events11.jpg',
+        }, {
+            'src': './images/events/events12.jpg',
+        }, {
+            'src': './images/events/events13.jpg',
+        }, {
+            'src': './images/events/events14.jpg',
+        }, {
+            'src': './images/events/events15.jpg',
+        }, {
+            'src': './images/events/events16.jpg',
+        }, {
+            'src': './images/events/events17.jpg',
+        }, {
+            'src': './images/events/events18.jpg',
+        }, {
+            'src': './images/events/events19.jpg',
+        }, {
+            'src': './images/events/events20.jpg',
+        }, {
+            'src': './images/events/events21.jpg',
+        }, {
+            'src': './images/events/events22.jpg',
+        }, {
+            'src': './images/events/events23.jpg',
+        }]
+    })
+
+});
+
+document.getElementById('wedding-gallery').addEventListener('click', function () {
+    lightGallery(document.getElementById('wedding-gallery'), {
+        dynamic: true,
+        dynamicEl: [{
+            "src": './images/wedding/wedding1.jpg',
+        }, {
+            'src': './images/wedding/wedding2.jpg',
+        }, {
+            'src': './images/wedding/wedding3.jpg',
+        }, {
+            'src': './images/wedding/wedding4.jpg',
+        }, {
+            'src': './images/wedding/wedding5.jpg',
+        }, {
+            'src': './images/wedding/wedding6.jpg',
+        }, {
+            'src': './images/wedding/wedding7.jpg',
+        }, {
+            'src': './images/wedding/wedding8.jpg',
+        }, {
+            'src': './images/wedding/wedding9.jpg',
+        }, {
+            'src': './images/wedding/wedding10.jpg',
+        }, {
+            'src': './images/wedding/wedding11.jpg',
+        }, {
+            'src': './images/wedding/wedding12.jpg',
+        }, {
+            'src': './images/wedding/wedding13.jpg',
+        }, {
+            'src': './images/wedding/wedding14.jpg',
+        }, {
+            'src': './images/wedding/wedding15.jpg',
+        }, {
+            'src': './images/wedding/wedding16.jpg',
+        }, {
+            'src': './images/wedding/wedding17.jpg',
+        }, {
+            'src': './images/wedding/wedding18.jpg',
+        }, {
+            'src': './images/wedding/wedding19.jpg',
+        }, {
+            'src': './images/wedding/wedding20.jpg',
+        }]
+    })
+
+});
+
+document.getElementById('books-gallery').addEventListener('click', function () {
+    lightGallery(document.getElementById('books-gallery'), {
+        dynamic: true,
+        dynamicEl: [{
+            "src": './images/books/books1.jpg',
+        }, {
+            'src': './images/books/books2.jpg',
+        }, {
+            'src': './images/books/books3.jpg',
+        }, {
+            'src': './images/books/books4.jpg',
+        }, {
+            'src': './images/books/books5.jpg',
+        }, {
+            'src': './images/books/books6.jpg',
+        }, {
+            'src': './images/books/books7.jpg',
+        }, {
+            'src': './images/books/books8.jpg',
+        }, {
+            'src': './images/books/books9.jpg',
+        }, {
+            'src': './images/books/books10.jpg',
+        }, {
+            'src': './images/books/books11.jpg',
+        }, {
+            'src': './images/books/books12.jpg',
+        }, {
+            'src': './images/books/books13.jpg',
+        }, {
+            'src': './images/books/books14.jpg',
+        }, {
+            'src': './images/books/books15.jpg',
+        }, {
+            'src': './images/books/books16.jpg',
+        }, {
+            'src': './images/books/books17.jpg',
+        }, {
+            'src': './images/books/books18.jpg',
+        }, {
+            'src': './images/books/books19.jpg',
+        }, {
+            'src': './images/books/books20.jpg',
         }]
     })
 
